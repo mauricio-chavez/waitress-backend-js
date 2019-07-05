81707261f0855cf8999f4de62b8eed2d1d565d10
Mauricio Chávez Olea <mauriciochavez@ciencias.unam.mx>
2019-07-04 21:51:49 -0500

    Heroku Deploy 2

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 // configura las variables de entorno para development
-require('dotenv').config();
+const NODE_ENV = process.env.NODE_ENV || "dev";
+
+if (NODE_ENV=="dev") {
+  require('dotenv').config();
+}
 
 // importacion de paquetes.
 const { ApolloServer } = require('apollo-server');
